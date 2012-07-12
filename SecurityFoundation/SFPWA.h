8800ad775450b0d03bc81222e86985d33670ea9e
Anders Bergh <anders1@gmail.com>
2012-07-12 11:42:05 +0200

    Commented the header. Added the OS X names for the algorithms.

--- SecurityFoundation/SFPWA.h
+++ SecurityFoundation/SFPWA.h
@@ -1,14 +1,15 @@
-/* Link with SecurityFoundation.framework */
+// Link with SecurityFoundation.framework
 #include <CoreFoundation/CoreFoundation.h>
 
 typedef struct SFPWAContext SFPWAContext;
 
 typedef enum {
-    SFPWAAlgorithmMemorable = 0,
-    SFPWAAlgorithmRandom = 1,
-    SFPWAAlgorithmLetters = 2,
-    SFPWAAlgorithmAlphanumeric = 3,
-    SFPWAAlgorithmNumbers = 4
+    SFPWAAlgorithmMemorable = 0, // Memorable
+    SFPWAAlgorithmRandom = 1, // Random
+    SFPWAAlgorithmLetters = 2, // FIPS-181 compliant
+    SFPWAAlgorithmAlphanumeric = 3, // Letters & Numbers
+    SFPWAAlgorithmNumbers = 4 // Numbers Only
+
     // These do nothing:
     // SFPWAAlgorithmUnknown = 5,
     // SFPWAAlgorithmManual = 6
@@ -17,5 +18,5 @@ typedef enum {
 SFPWAContext *SFPWAContextCreate(void);
 SFPWAContext *SFPWAContextCreateWithDefaults(void);
 CFDictionaryRef SFPWAPolicyCopyDefault(void);
-CFMutableArrayRef SFPWAPasswordSuggest(SFPWAContext *ctx, CFDictionaryRef policy, int desiredLength, int unknown, int count, SFPWAAlgorithm algorithm);
+CFMutableArrayRef SFPWAPasswordSuggest(SFPWAContext *ctx, CFDictionaryRef policy, int length, int unknown, int count, SFPWAAlgorithm algorithm);
 void *SFPWAContextRelease(SFPWAContext *ctx);
