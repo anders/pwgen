1780b09027f51ce71411a21872ba884eaac83f05
Anders Bergh <anders1@gmail.com>
2013-03-02 05:58:29 +0100

    Port pwgen to Objective-C.

--- pwgen.m
+++ pwgen.m
@@ -2,23 +2,23 @@
  * pwgen.c -- OS X command line password generator
  *
  * Copyright (c) 2013 Anders Bergh <anders1@gmail.com>
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  *   1. The origin of this software must not be misrepresented; you must not
  *   claim that you wrote the original software. If you use this software
  *   in a product, an acknowledgment in the product documentation would be
  *   appreciated but is not required.
- * 
+ *
  *   2. Altered source versions must be plainly marked as such, and must
  *   not be misrepresented as being the original software.
- * 
+ *
  *   3. This notice may not be removed or altered from any source
  *   distribution.
  */
@@ -29,9 +29,8 @@
 #include <getopt.h>
 #include <assert.h>
 
-#include <CoreFoundation/CoreFoundation.h>
-
-#include "SecurityFoundation/SFPasswordAssistant.h"
+#import <Foundation/Foundation.h>
+#import "SecurityFoundation/SFPasswordAssistant.h"
 
 #define MIN_LENGTH 8
 #define MAX_LENGTH 31
@@ -52,7 +51,7 @@ int main (int argc, char *argv[]) {
     int count = 1;
     int length = 12;
     SFPWAAlgorithm algorithm = kSFPWAAlgorithmMemorable;
-
+    
     const struct option longopts[] = {
         { "algorithm", optional_argument, NULL, 'a' },
         { "count",     required_argument, NULL, 'c' },
@@ -60,81 +59,70 @@ int main (int argc, char *argv[]) {
         { "help",      no_argument,       NULL, 'h' },
         { NULL,        0,                 NULL, 0   }
     };
-
+    
     char ch;
     while ((ch = getopt_long(argc, argv, "c:a:l:h", longopts, NULL)) != -1) {
         switch (ch) {
             case 'a':
                 if (strcmp(optarg, "memorable") == 0)
                     algorithm = kSFPWAAlgorithmMemorable;
-
+                
                 else if (strcmp(optarg, "random") == 0)
                     algorithm = kSFPWAAlgorithmRandom;
-
+                
                 else if (strcmp(optarg, "letters") == 0)
                     algorithm = kSFPWAAlgorithmLetters;
-
+                
                 else if (strcmp(optarg, "alphanumeric") == 0)
                     algorithm = kSFPWAAlgorithmAlphanumeric;
-
+                
                 else if (strcmp(optarg, "numbers") == 0)
                     algorithm = kSFPWAAlgorithmNumbers;
-
+                
                 else {
                     fprintf(stderr, "%s: error: unknown algorithm.\n", argv[0]);
                     usage(argv[0]);
                     return 1;
                 }
                 break;
-
+                
             case 'c':
                 count = atoi(optarg);
                 break;
-
+                
             case 'l':
                 length = atoi(optarg);
                 break;
-
+                
             default:
                 usage(argv[0]);
                 return 1;
         }
     }
-
+    
     if (count < 1)
         count = 1;
     
     if (length < MIN_LENGTH)
         length = MIN_LENGTH;
-
+    
     else if (length > MAX_LENGTH)
         length = MAX_LENGTH;
-
+    
     SFPWAContextRef ctx = SFPWAContextCreateWithDefaults();
     assert(ctx != NULL);
     
-    CFDictionaryRef policy = SFPWAPolicyCopyDefault();
+    NSDictionary *policy = (NSDictionary *)SFPWAPolicyCopyDefault();
     assert(policy != NULL);
     
-    CFMutableArrayRef suggestions = SFPWAPasswordSuggest(ctx, policy, length, 0, count, algorithm);
+    NSMutableArray *suggestions = (NSMutableArray *)SFPWAPasswordSuggest(ctx, (CFDictionaryRef)policy, length, 0, count, algorithm);
     assert(suggestions != NULL);
     
-    for (int i = 0; i < CFArrayGetCount(suggestions); i++) {
-        CFStringRef s = CFArrayGetValueAtIndex(suggestions, i);
-        assert(s != NULL);
-        
-        long n = CFStringGetMaximumSizeForEncoding(CFStringGetLength(s), kCFStringEncodingUTF8) + 1;
-        char *buf = malloc(n);
-        assert(buf != NULL);
-        
-        if (CFStringGetCString(s, buf, n, kCFStringEncodingUTF8))
-            printf("%s\n", buf);
-
-        free(buf);
-    }
-
+    for (NSString *s in suggestions)
+        printf("%s\n", [s UTF8String]);
+    
     SFPWAContextRelease(ctx);
-    CFRelease(policy);
-
+    [policy release];
+    
     return 0;
 }
