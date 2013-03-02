b9a7015863f829aeb05c9e60242066429834e87d
Anders Bergh <anders1@gmail.com>
2013-03-02 05:23:48 +0100

    SFPasswordAssistant.h:   add include guard   remove superfluous comments   add URL to the bitbucket repository   add public domain notice

--- SecurityFoundation/SFPasswordAssistant.h
+++ SecurityFoundation/SFPasswordAssistant.h
@@ -1,14 +1,23 @@
-// Link with SecurityFoundation.framework
+/*
+ * Public domain
+ *
+ * Written by Anders Bergh <anders1@gmail.com>
+ * Repository at https://bitbucket.org/anders/securityfoundation/
+ */
+
+#ifndef SFPASSWORDASSISTANT_H
+#define SFPASSWORDASSISTANT_H
+
 #include <CoreFoundation/CoreFoundation.h>
 
 typedef struct SFPWAContext * SFPWAContextRef;
 
 typedef enum {
-    kSFPWAAlgorithmMemorable = 0, // Memorable
-    kSFPWAAlgorithmRandom = 1, // Random
-    kSFPWAAlgorithmLetters = 2, // FIPS-181 compliant
-    kSFPWAAlgorithmAlphanumeric = 3, // Letters & Numbers
-    kSFPWAAlgorithmNumbers = 4 // Numbers Only
+    kSFPWAAlgorithmMemorable = 0,
+    kSFPWAAlgorithmRandom = 1,
+    kSFPWAAlgorithmLetters = 2, /* FIPS-181 compliant */
+    kSFPWAAlgorithmAlphanumeric = 3,
+    kSFPWAAlgorithmNumbers = 4
 } SFPWAAlgorithm;
 
 bool SFPWACStringInDictionary(SFPWAContextRef ctx, const char *string);
@@ -16,21 +25,20 @@ SFPWAContextRef SFPWAContextCreate(void);
 SFPWAContextRef SFPWAContextCreateWithDefaults(void);
 void SFPWAContextLoadDictionaries(SFPWAContextRef ctx, CFArrayRef langs, bool unknown);
 void SFPWAContextRelease(SFPWAContextRef ctx);
-CFDictionaryRef SFPWAPasswordEvaluator(SFPWAContextRef ctx, CFStringRef password, int unknown, CFDictionaryRef policy);
-/*
- * Example result:
+
+/* Example:
+ *
+ * SFPWAPasswordEvaluator(ctx, CFSTR("password"), 0, policy) ->
  * {
- *     "Entropy": "136.9465151388981",
- *     "Quality": 80,
- *     "Variety": 3,
- *     "Errors": [
- *         {
- *             "Message": "Mix upper and lower case, punctuation, and numbers",
- *             "Value": "-27099"
- *         }
- *     ]
+ *   "Entropy": "123.4567",
+ *   "Quality": 80,
+ *   "Variety": 3,
+ *   "Errors": [
+ *     { "Value": "-27099", "Message": "Mix upper and lower case, punctuation, and numbers" }
+ *   ]
  * }
  */
+CFDictionaryRef SFPWAPasswordEvaluator(SFPWAContextRef ctx, CFStringRef password, int unknown, CFDictionaryRef policy);
 
 CFMutableArrayRef SFPWAPasswordSuggest(SFPWAContextRef ctx, CFDictionaryRef policy, int length, int unknown, int count, SFPWAAlgorithm algorithm);
 CFDictionaryRef SFPWAPolicyCopyDefault(void);
@@ -38,3 +46,5 @@ CFPropertyListRef SFPWAPolicyCopyFromFile(const char *path);
 CFArrayRef SFPWAPolicyLanguages(CFDictionaryRef policy);
 void SFPWAPolicyParse(SFPWAContextRef ctx, CFDictionaryRef policy);
 CFStringRef SFPWARandomWord(SFPWAContextRef ctx, unsigned int length);
+
+#endif /* SFPASSWORDASSISTANT_H */
