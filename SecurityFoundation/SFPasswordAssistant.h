e1f838262113fac1b677f3c578cb30c477e1d24c
Anders Bergh <anders1@gmail.com>
2013-08-31 02:50:18 +0200

    Reformat using clang-format, Mozilla style.

--- SecurityFoundation/SFPasswordAssistant.h
+++ SecurityFoundation/SFPasswordAssistant.h
@@ -10,20 +10,21 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 
-typedef struct SFPWAContext * SFPWAContextRef;
+typedef struct SFPWAContext *SFPWAContextRef;
 
 typedef enum {
-    kSFPWAAlgorithmMemorable = 0,
-    kSFPWAAlgorithmRandom = 1,
-    kSFPWAAlgorithmLetters = 2, /* FIPS-181 compliant */
-    kSFPWAAlgorithmAlphanumeric = 3,
-    kSFPWAAlgorithmNumbers = 4
+  kSFPWAAlgorithmMemorable = 0,
+  kSFPWAAlgorithmRandom = 1,
+  kSFPWAAlgorithmLetters = 2, /* FIPS-181 compliant */
+  kSFPWAAlgorithmAlphanumeric = 3,
+  kSFPWAAlgorithmNumbers = 4
 } SFPWAAlgorithm;
 
 bool SFPWACStringInDictionary(SFPWAContextRef ctx, const char *string);
 SFPWAContextRef SFPWAContextCreate(void);
 SFPWAContextRef SFPWAContextCreateWithDefaults(void);
-void SFPWAContextLoadDictionaries(SFPWAContextRef ctx, CFArrayRef langs, bool unknown);
+void SFPWAContextLoadDictionaries(SFPWAContextRef ctx, CFArrayRef langs,
+                                  bool unknown);
 void SFPWAContextRelease(SFPWAContextRef ctx);
 
 /* Example:
@@ -34,13 +35,19 @@ void SFPWAContextRelease(SFPWAContextRef ctx);
  *   "Quality": 80,
  *   "Variety": 3,
  *   "Errors": [
- *     { "Value": "-27099", "Message": "Mix upper and lower case, punctuation, and numbers" }
+ *     { "Value": "-27099", "Message": "Mix upper and lower case, punctuation,
+ * and numbers" }
  *   ]
  * }
  */
-CFDictionaryRef SFPWAPasswordEvaluator(SFPWAContextRef ctx, CFStringRef password, int unknown, CFDictionaryRef policy);
+CFDictionaryRef SFPWAPasswordEvaluator(SFPWAContextRef ctx,
+                                       CFStringRef password, int unknown,
+                                       CFDictionaryRef policy);
 
-CFMutableArrayRef SFPWAPasswordSuggest(SFPWAContextRef ctx, CFDictionaryRef policy, int length, int unknown, int count, SFPWAAlgorithm algorithm);
+CFMutableArrayRef SFPWAPasswordSuggest(SFPWAContextRef ctx,
+                                       CFDictionaryRef policy, int length,
+                                       int unknown, int count,
+                                       SFPWAAlgorithm algorithm);
 CFDictionaryRef SFPWAPolicyCopyDefault(void);
 CFPropertyListRef SFPWAPolicyCopyFromFile(const char *path);
 CFArrayRef SFPWAPolicyLanguages(CFDictionaryRef policy);
--- pwgen.m
+++ pwgen.m
@@ -36,94 +36,94 @@
 #define MAX_LENGTH 31
 
 static void usage(const char *argv0) {
-    printf("usage: %s [options]\n\n", argv0);
-    printf("Option:          Meaning:\n");
-    printf("  -c, --count      The number of passwords to generate.\n");
-    printf("  -a, --algorithm  Available algorithms: memorable, random\n");
-    printf("                   letters, alphanumeric, numbers.\n");
-    printf("                   The default is `memorable'.\n");
-    printf("  -l, --length     Desired length of the generated passwords.\n");
-    printf("  -h, --help       Prints this message.\n");
-    exit(1);
+  printf("usage: %s [options]\n\n", argv0);
+  printf("Option:          Meaning:\n");
+  printf("  -c, --count      The number of passwords to generate.\n");
+  printf("  -a, --algorithm  Available algorithms: memorable, random\n");
+  printf("                   letters, alphanumeric, numbers.\n");
+  printf("                   The default is `memorable'.\n");
+  printf("  -l, --length     Desired length of the generated passwords.\n");
+  printf("  -h, --help       Prints this message.\n");
+  exit(1);
 }
 
-int main (int argc, char *argv[]) {
-    // Default options
-    int count = 1;
-    int length = 12;
-    SFPWAAlgorithm algorithm = kSFPWAAlgorithmMemorable;
-    
-    const struct option longopts[] = {
-        { "algorithm", optional_argument, NULL, 'a' },
-        { "count",     required_argument, NULL, 'c' },
-        { "length",    required_argument, NULL, 'l' },
-        { "help",      no_argument,       NULL, 'h' },
-        { NULL,        0,                 NULL, 0   }
-    };
-    
-    char ch;
-    while ((ch = getopt_long(argc, argv, "c:a:l:h", longopts, NULL)) != -1) {
-        switch (ch) {
-            case 'a':
-                if (strcmp(optarg, "memorable") == 0)
-                    algorithm = kSFPWAAlgorithmMemorable;
-                
-                else if (strcmp(optarg, "random") == 0)
-                    algorithm = kSFPWAAlgorithmRandom;
-                
-                else if (strcmp(optarg, "letters") == 0)
-                    algorithm = kSFPWAAlgorithmLetters;
-                
-                else if (strcmp(optarg, "alphanumeric") == 0)
-                    algorithm = kSFPWAAlgorithmAlphanumeric;
-                
-                else if (strcmp(optarg, "numbers") == 0)
-                    algorithm = kSFPWAAlgorithmNumbers;
-                
-                else {
-                    fprintf(stderr, "%s: error: unknown algorithm.\n", argv[0]);
-                    usage(argv[0]);
-                    return 1;
-                }
-                break;
-                
-            case 'c':
-                count = atoi(optarg);
-                break;
-                
-            case 'l':
-                length = atoi(optarg);
-                break;
-                
-            default:
-                usage(argv[0]);
-                return 1;
+int main(int argc, char *argv[]) {
+  // Default options
+  int count = 1;
+  int length = 12;
+  SFPWAAlgorithm algorithm = kSFPWAAlgorithmMemorable;
+
+  const struct option longopts[] = {
+    { "algorithm", optional_argument, NULL, 'a' },
+    { "count", required_argument, NULL, 'c' },
+    { "length", required_argument, NULL, 'l' },
+    { "help", no_argument, NULL, 'h' }, { NULL, 0, NULL, 0 }
+  };
+
+  char ch;
+  while ((ch = getopt_long(argc, argv, "c:a:l:h", longopts, NULL)) != -1) {
+    switch (ch) {
+      case 'a':
+        if (strcmp(optarg, "memorable") == 0)
+          algorithm = kSFPWAAlgorithmMemorable;
+
+        else if (strcmp(optarg, "random") == 0)
+          algorithm = kSFPWAAlgorithmRandom;
+
+        else if (strcmp(optarg, "letters") == 0)
+          algorithm = kSFPWAAlgorithmLetters;
+
+        else if (strcmp(optarg, "alphanumeric") == 0)
+          algorithm = kSFPWAAlgorithmAlphanumeric;
+
+        else if (strcmp(optarg, "numbers") == 0)
+          algorithm = kSFPWAAlgorithmNumbers;
+
+        else {
+          fprintf(stderr, "%s: error: unknown algorithm.\n", argv[0]);
+          usage(argv[0]);
+          return 1;
         }
+        break;
+
+      case 'c':
+        count = atoi(optarg);
+        break;
+
+      case 'l':
+        length = atoi(optarg);
+        break;
+
+      default:
+        usage(argv[0]);
+        return 1;
     }
-    
-    if (count < 1)
-        count = 1;
-    
-    if (length < MIN_LENGTH)
-        length = MIN_LENGTH;
-    
-    else if (length > MAX_LENGTH)
-        length = MAX_LENGTH;
-    
-    SFPWAContextRef ctx = SFPWAContextCreateWithDefaults();
-    assert(ctx != NULL);
-    
-    NSDictionary *policy = (NSDictionary *)SFPWAPolicyCopyDefault();
-    assert(policy != NULL);
-    
-    NSMutableArray *suggestions = (NSMutableArray *)SFPWAPasswordSuggest(ctx, (CFDictionaryRef)policy, length, 0, count, algorithm);
-    assert(suggestions != NULL);
-    
-    for (NSString *s in suggestions)
-        printf("%s\n", [s UTF8String]);
-    
-    SFPWAContextRelease(ctx);
-    [policy release];
-    
-    return 0;
+  }
+
+  if (count < 1)
+    count = 1;
+
+  if (length < MIN_LENGTH)
+    length = MIN_LENGTH;
+
+  else if (length > MAX_LENGTH)
+    length = MAX_LENGTH;
+
+  SFPWAContextRef ctx = SFPWAContextCreateWithDefaults();
+  assert(ctx != NULL);
+
+  NSDictionary *policy = (NSDictionary *)SFPWAPolicyCopyDefault();
+  assert(policy != NULL);
+
+  NSMutableArray *suggestions = (NSMutableArray *)SFPWAPasswordSuggest(
+      ctx, (CFDictionaryRef) policy, length, 0, count, algorithm);
+  assert(suggestions != NULL);
+
+  for (NSString *s in suggestions)
+    printf("%s\n", [s UTF8String]);
+
+  SFPWAContextRelease(ctx);
+  [policy release];
+
+  return 0;
 }
